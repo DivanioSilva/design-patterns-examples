200919f934346f4a8e49616b1149aa9c25041dff
Divanio Silva <dcsilva@mail.ru>
2020-06-28 22:45:53 +0100

    backup

--- src/main/java/com/ds/designpatternsexamples/pluralSight/factory/Definitions.java
+++ src/main/java/com/ds/designpatternsexamples/pluralSight/factory/Definitions.java
@@ -1,4 +1,19 @@
 package com.ds.designpatternsexamples.pluralSight.factory;
 
+import java.util.List;
+
 public class Definitions {
+    private List<Definition> definitions;
+
+    public Definitions(List<Definition> definitions) {
+        this.definitions = definitions;
+    }
+
+    public List<Definition> getDefinitions() {
+        return definitions;
+    }
+
+    public void setDefinitions(List<Definition> definitions) {
+        this.definitions = definitions;
+    }
 }
