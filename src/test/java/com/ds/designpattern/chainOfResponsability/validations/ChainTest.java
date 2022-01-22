b47b3b8497d3088ef514c2d4cd30c54898acef63
Divanio Silva <dcsilva@mail.ru>
2022-01-22 22:59:36 +0100

    Add more tests

--- src/test/java/com/ds/designpattern/chainOfResponsability/validations/ChainTest.java
+++ src/test/java/com/ds/designpattern/chainOfResponsability/validations/ChainTest.java
@@ -2,7 +2,9 @@ package com.ds.designpattern.chainOfResponsability.validations;
 
 import com.ds.entities.Employee;
 import com.ds.exceptions.ParamException;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -34,60 +36,81 @@ class ChainTest {
     }
 
     @Test
+    @DisplayName("nullListTest")
     void nullListTest(){
         ParamException exception = assertThrows(ParamException.class, () -> {
             validationChain.validate(null);
-        }, "Employee list is null or empty.");
+        });
 
         assertEquals("Employee list is null or empty.", exception.getMessage());
     }
 
     @Test
+    @DisplayName("emptyListTest")
     void emptyListTest(){
         ParamException exception = assertThrows(ParamException.class, () -> {
             validationChain.validate(Collections.EMPTY_LIST);
-        }, "Employee list is null or empty.");
+        });
 
         assertEquals("Employee list is null or empty.", exception.getMessage());
     }
 
     @Test
+    @DisplayName("employeeIsNullTest")
     void employeeIsNullTest(){
         employees.add(null);
         ParamException exception = assertThrows(ParamException.class, () -> {
             validationChain.validate(employees);
-        }, "Employee null.");
+        });
 
         assertEquals("Employee null.", exception.getMessage());
     }
 
     @Test
+    @DisplayName("employeeIdIsNullTest")
     void employeeIdIsNullTest(){
         employees.add(Employee.builder().build());
         ParamException exception = assertThrows(ParamException.class, () -> {
             validationChain.validate(employees);
-        }, "Employee id is null or empty.");
+        });
 
         assertEquals("Employee id is null or empty.", exception.getMessage());
     }
 
     @Test
+    @DisplayName("employeeFirstNameEmptyTest")
     void employeeFirstNameEmptyTest(){
         employees.add(Employee.builder().id(1L).build());
         ParamException exception = assertThrows(ParamException.class, () -> {
             validationChain.validate(employees);
-        }, "Employee first name is null or empty.");
+        });
 
         assertEquals("Employee first name is null or empty.", exception.getMessage());
     }
 
     @Test
+    @DisplayName("employeeLastNameEmptyTest")
     void employeeLastNameEmptyTest(){
         employees.add(Employee.builder().id(1L).firstName("Mianna").build());
         ParamException exception = assertThrows(ParamException.class, () -> {
             validationChain.validate(employees);
-        }, "Employee last name is null or empty.");
+        });
 
         assertEquals("Employee last name is null or empty.", exception.getMessage());
     }
+
+    /*
+    @Test
+    void okTest() throws ParamException {
+        employees.add(Employee.builder().id(1L).firstName("Mianna").lastName("Silva Liberati").build());
+        assertDoesNotThrow(ParamException.class, () -> {
+            validationChain.validate(employees);
+        });
+        //assertDoesNotThrow(ParamException.class, () -> {
+        //    validationChain.validate(employees);
+        //});
+
+    }
+
+     */
 }
